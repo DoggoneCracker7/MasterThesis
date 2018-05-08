fe37294f5e684df3f148e3686a49147a7b714b7f
DoggoneCracker7 <przemek.c93@wp.pl>
2018-05-08 22:05:07 +0200

    Package transfer implementation - utils

---
+++ src/main/java/com/pl/masterthesis/utils/ConnectionPool.java
@@ -0,0 +1,38 @@
+package com.pl.masterthesis.utils;
+
+import com.pl.masterthesis.models.Connection;
+import com.pl.masterthesis.models.Interface;
+
+import java.util.*;
+
+public final class ConnectionPool {
+    private static final ConnectionPool INSTANCE = new ConnectionPool();
+    private List<Connection> connections;
+
+    public static ConnectionPool get() {
+        return INSTANCE;
+    }
+
+    private ConnectionPool() {
+        this.connections = new ArrayList<>();
+    }
+
+    public void addConnection(Connection... connection) {
+        connections.addAll(Arrays.asList(connection));
+    }
+
+    public Optional<Connection> getConnection(Interface sourceInterface) {
+        Objects.requireNonNull(sourceInterface, "sourceInterface cannot be null");
+        for (Connection connection : connections) {
+            if (connactionContainsInterface(connection, sourceInterface)) {
+                return Optional.of(connection);
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean connactionContainsInterface(Connection connection, Interface sourceInterface) {
+        Objects.requireNonNull(sourceInterface, "sourceInterface cannot be null");
+        return sourceInterface.equals(connection.getFirstInterface()) || sourceInterface.equals(connection.getSecondInterface());
+    }
+}
